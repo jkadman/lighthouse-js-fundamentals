1b1603c6d637d6c22ebf5bc5a9cde2d0020d22f4
Jason Kadman <jkadman231@gmail.com>
2022-08-14 08:09:11 +0000

    not sure what the changes are

--- tinkering.js
+++ tinkering.js
@@ -1,28 +1,15 @@
+
+const amounts = [61.00, 52.25, 112.99, 5.00];
 /*
- * Programming Quiz: Find my Seat (4-8)
- * 
- * Write a nested for loop to print out all of the different seat combinations in the theater.
- * The first row-seat combination should be 0-0 
- * The last row-seat combination will be 25-99
- * 
- * Things to note: 
- *  - the row and seat numbers start at 0, not 1
- *  - the highest seat number is 99, not 100
- */
-
-/*
- * QUIZ REQUIREMENTS
- * - Your code should use a nested `for` loop'
- * - Your code should produce the expected output, as explained above
- */
- 
-// Write your code here
-
-let row = 0;
-let seat = 0;
-
-for (row = 0; row <= 25; row++) {
-  for (seat = 0; seat < 100; seat++) {
-    console.log(row + '-' + seat);
-  }
+let total = 0;
+for (let i = 0; i < amounts.length; i++) {
+  total += amounts[i];
 }
+console.log('Order total is: ', total);
+*/
+
+let total = 0;
+for (let amount of amounts) {
+  total += amount;
+}
+console.log('Order total is: ', total);
