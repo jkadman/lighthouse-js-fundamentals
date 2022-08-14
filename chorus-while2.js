90aac89cc2782139360c5276c9be42ff12bdb601
Jason Kadman <jkadman231@gmail.com>
2022-08-14 08:05:32 +0000

    some refresher material after a bit of a break

---
+++ chorus-while2.js
@@ -0,0 +1,16 @@
+const chorus = 'Let\'s dance';
+
+let repeat = 0;
+while (repeat < 10) {
+
+  if (repeat % 2 === 0) {
+    console.log('yeah baby yeah!!!');
+  } else if (repeat === 7) {
+     
+      console.log('boom ba boom');
+    
+  }
+  console.log(chorus);
+  repeat++;
+}  
+console.log('Until the sun comes up!');
