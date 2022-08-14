d7d4183c58a21bb38ec20ea78794e8445acb66a6
Jason Kadman <jkadman231@gmail.com>
2022-08-14 20:52:57 +0000

    udacity functions quiz 2

--- tinkering.js
+++ tinkering.js
@@ -1,8 +1,30 @@
-function findAverage(x, y) {
-  var answer = (x + y) / 2;
-  return answer;
+/*
+ * Programming Quiz: Laugh it Off 2 (5-2)
+ *
+ * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+ *
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ *  - make sure that your function produces the correct results when it is called multiple times
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `laugh()` function
+ * - Your `laugh()` function should have one parameter named `num`
+ * - Your `laugh()` function should return the correct number of laughs
+ */
+
+let sound = "";
+
+function laugh(num) {
+  for (let x = 0; x < num; x++) {
+    sound = sound + 'Ha';
+  }
+  sound = sound + '!';
+  return sound;
 }
 
-var avg = findAverage(5, 9);
+console.log(laugh(8));
 
-console.log(avg);
+
