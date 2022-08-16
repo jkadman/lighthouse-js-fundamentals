c00ad17554c3dc1c00086cb86cb02c5640796ccc
Jason Kadman <jkadman231@gmail.com>
2022-08-16 07:53:21 +0000

    functionsgit add hello.js

---
+++ hello.js
@@ -0,0 +1,11 @@
+const sayHello = function (name) {
+  console.log('Hello ' + name);
+}
+
+sayHello('Jack');
+
+const sayHello2 = function (name) {
+  return 'Hello ' + name;
+}
+
+console.log(sayHello2('Jack'));
