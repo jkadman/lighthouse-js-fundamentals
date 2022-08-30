74ab9f01d03ca4f54547e6bd65a3ef73ad4f9fb9
Jason Kadman <jkadman231@gmail.com>
2022-08-30 03:18:44 +0000

    working through the LHL fundamentals

--- choosestations.js
+++ choosestations.js
@@ -1,52 +1,32 @@
-/*const stations = [
-  ['Chef Rons', 30, 'restaurant'],
-  ['Britannia', 40, 'community centre'],
-  ['Lord Bing', 100, 'school'],
-  ['Best Falafel', 10, 'restaurant'],
-  ['Vantech', 100, 'school'],
-]; */
+/*
+ * Programming Quiz: Nested Numbers (6-10)
+ * QUIZ REQUIREMENTS
+ *   - The `numbers` variable is an array of arrays.
+ *   - Use a nested `for` loop to cycle through `numbers`.
+ *   - Convert each even number to the string "even"
+ *   - Convert each odd number to the string "odd"
+ */
 
-const stations = [
-  ['Big Bear Donair', 10, 'restaurant'],
-  ['Bright Lights Elementary', 50, 'school'],
-  ['Moose Mountain Community Centre', 45, 'community centre'],
+let numbers = [
+  [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+  [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+  [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+  [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+  [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+  [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+  [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+  [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+  [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+  [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
 ];
 
-
-
-
-//let goodStations = [];
-
-
-
-/*function chooseStations (stations) {
-  for (const station of stations) {
-   if (station[2] === 'School' || station[2] === 'Community Centre' 
-        && station[1] >= 20 ) {
-          console.log(station[0])
-        } else {
-          return console.log(station[0]);
-        };
-      } 
+for (let row = 0; row < numbers.length; row++) {
+  for (let column = 0; column < numbers[row].length; column++) {
+    if (numbers[row][column] % 2 === 0) {
+      console.log('even');
+    } else {
+      console.log('odd');
     }
+  } 
+} 
 
-console.log(chooseStations(stations));
-*/
-
-
-function chooseStations (stations) {
-  let goodStations = [];
-  //let badStations = [];
-  for (const station of stations) {
-    const capacity = station[1];
-    const type = station[2];
-    if ((capacity >= 20) && (type === 'school' || type === 'community centre')) {
-      goodStations.push(station[0])
-    } /* else {
-      badStations.push(station[0]);
-    } */
-  }
-  return goodStations; 
-}
-
-console.log(chooseStations(stations));
---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+iceCreamFlavours.slice('root beer');
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours.slice(0,1));
+console.log(iceCreamFlavours[0]);
+
+console.log('15' === 15)
+
+console.log("Bob" || " Marley")
+
+console.log("Don\nBurks")
